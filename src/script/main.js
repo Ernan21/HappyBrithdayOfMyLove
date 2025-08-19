e2d186f76d9cddcdcd7740db2a26f1398a1be57c
Ernando Freitas <ernando.freitas123@Gmail.com>
2025-08-19 02:18:13 -0300

    iniciando Projeto
    Apenas Iniciando o Projeto com o template inicial do JS

---
+++ src/script/main.js
@@ -0,0 +1,9 @@
+const year_start = "ano_inicio_namoro"
+const month_start = "mes_inicio_namoro"
+
+function setTemoRelacionamento(){
+    id_Tempo_Namoro = document.getElementById('AlertTimer')
+    id_Tempo_Namoro.innnerText = 'Test'
+}
+
+setTemoRelacionamento()
